a1cd44c42f12eb908b3ddb73bb61de3588ee013a
Piotr <elfaltino@gmail.com>
2021-04-26 08:45:36 +0000

    next commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('startuje app.js')
 console.log('testowy output')
 console.log('trawa jest zielona')
 console.log('niebo jest niebieskie')
---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,24 @@
+job ('nodejs job')
+{
+    scm{
+        git('https://github.com/faltypi/githubfaltypi2.git'){
+            node / gitConfigName('DSL script')
+            node / gitConfigEmail('jenkins@fakeemail.com')
+
+        }
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+
+    }
+
+    wrappers{
+        nodejs('nodejs')
+    }
+
+    steps{
+        shell('node app.js')
+    }
+
+}
